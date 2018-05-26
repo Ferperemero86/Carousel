424437e4f216d20c625c1b1300a50c3631fdd9bb
Ferperemero86 <fernandoperezmerono@gmail.com>
2018-05-26 23:54:27 +0100

    Organize code

--- scripts/main.js
+++ scripts/main.js
@@ -1,85 +1,86 @@
  $(document).ready(function() {
 
-  //<---Model--->
+    //<---Model--->
 
-  var images = {
-   currentImage:'images/01.jpg',
-   imagesList:[
-  {
-    name:'first',
-    source:'images/01.jpg'
-  },
-  {
-   name:'second',
-   source:'images/02.jpg'
-  },
-  {
-   name:'third',
-   source:'images/03.jpg'
- },
- {
-  name:'fourth',
-  source:'images/04.jpg'
- }
+   var images = {
+      currentImage:'images/01.jpg',
+      imagesList:[
+        {
+        name:'first',
+        source:'images/01.jpg'
+        },
+        {
+        name:'second',
+        source:'images/02.jpg'
+        },
+        {
+        name:'third',
+        source:'images/03.jpg'
+        },
+        {
+        name:'fourth',
+        source:'images/04.jpg'
+        }
 
-  ]
-};
+      ]
+    };
 
- var carousel = [
-  'images/01.jpg','images/02.jpg','images/03.jpg','images/04.jpg',
- ]
+   var carousel = [
+     'images/01.jpg','images/02.jpg','images/03.jpg','images/04.jpg',
+   ]
 
- //<---Content--->
+   //<---Content--->
 
-var content = {
-  getImages: function() {
-    return images.imagesList;
-  },
-  getImagesCarousel: function() {
-    return carousel;
-  },
-  setCurrentImage: function(image) {
-    images.currentImage = image;
-  },
-  getCurrentImage: function() {
-    return images.currentImage;
-  }
-};
+   var content = {
+      getImages: function() {
+         return images.imagesList;
+      },
+      getImagesCarousel: function() {
+         return carousel;
+      },
+      setCurrentImage: function(image) {
+         images.currentImage = image;
+      },
+      getCurrentImage: function() {
+         return images.currentImage;
+      }
+   };
 
- //<---View--->
+   //<---View--->
 
- var pos=0;
+   var pos=0;
 
-  //Print the carousel array images
+   //Print the carousel array images
 
-  $.each(carousel,function(index,value) {
-    var img = document.createElement("IMG");
-    $('#carousel').prepend(img);
-    var parentElement = document.getElementById('carousel');
-    parentElement.firstElementChild.src = value;
-  });
- // Set and print the main Image to show
+   $.each(carousel,function(index,value) {
+      var img = document.createElement("IMG");
+      $('#carousel').prepend(img);
+      var parentElement = document.getElementById('carousel');
+      parentElement.firstElementChild.src = value;
+   });
+  // Set and print the main Image to show
 
-  var mainImage = document.createElement("IMG");
-  $('#main').append(mainImage);
-  var parentElement = document.getElementById('main');
-  parentElement.lastElementChild.src = images.currentImage;
+   var mainImage = document.createElement("IMG");
+   $('#main').append(mainImage);
+   var parentElement = document.getElementById('main');
+   parentElement.lastElementChild.src = images.currentImage;
 
 
+   $('.next').click(function(e) {
+      e.preventDefault();
 
- $('.next').click(function(e) {
-   e.preventDefault();
+      var images = content.getImages();
 
-   var images = content.getImages();
+     //When click Update the position of the images
 
-   //When click Update the position of the images
-   if(pos === images.length - 1) {
-      pos = 0
-   } else {
-     pos++
-   }
+     if(pos === images.length - 1) {
+        pos = 0
+     } else {
+        pos++
+     }
 
    //Set current Image
+
    var setImage = images[pos].source;
    content.setCurrentImage(setImage);
    var currentImage = content.getCurrentImage();
@@ -87,41 +88,42 @@ var content = {
    parentElement.lastElementChild.src = currentImage;
 
    //Create fade efect
+
    $('#main img').hide();
    $('#main img').fadeIn(500,"swing");
 
    //Remove and load new carousel
+
    $('#carousel img').remove();
 
-  var carVal = carousel[0];
-  carousel.shift();
-  carousel.push(carVal);
-  console.log(carousel);
-  $.each(carousel,function(index,value) {
+   var carVal = carousel[0];
+   carousel.shift();
+   carousel.push(carVal)
+   $.each(carousel,function(index,value) {
 
-    var img = document.createElement("IMG");
-    $('#carousel').prepend(img);
-    var parentElement = document.getElementById('carousel');
-    parentElement.firstElementChild.src = value;
+      var img = document.createElement("IMG");
+      $('#carousel').prepend(img);
+      var parentElement = document.getElementById('carousel');
+      parentElement.firstElementChild.src = value;
 
-
-
-  });
+   });
 
  });//End CLICK
 
 
  $('.before').click(function(e) {
-   e.preventDefault();
-   var images = content.getImages();
-   console.log(images.length);
-   console.log(pos);
+    e.preventDefault();
+    var images = content.getImages();
+
    //When click Update the position of the images
+
       pos--;
       if(pos === -1) {
        pos=3;
       }
+
    //Set current Image
+
    var setImage = images[pos].source;
    content.setCurrentImage(setImage);
    var currentImage = content.getCurrentImage();
@@ -131,24 +133,22 @@ var content = {
    //Create fade effect
    $('#main img').hide();
    $('#main img').fadeIn(500,"swing");
-
    $('#carousel img').remove();
 
    //Remove and load new carousel
    var carVal = carousel[3];
    carousel.pop();
    carousel.unshift(carVal);
-   console.log(carousel);
+
    $.each(carousel,function(index,value) {
 
-     var img = document.createElement("IMG");
-     $('#carousel').prepend(img);
-     var parentElement = document.getElementById('carousel');
-     parentElement.firstElementChild.src = value;
+      var img = document.createElement("IMG");
+      $('#carousel').prepend(img);
+      var parentElement = document.getElementById('carousel');
+      parentElement.firstElementChild.src = value;
    });
 
- });//End click
-
+  });//End click
 
 
  }); //End READY
